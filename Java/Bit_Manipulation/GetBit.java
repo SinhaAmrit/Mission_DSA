3c462b9f88a86fcd2257f65eacf5a0f67bd31545
Amrit Sinha <purpleplots@gmail.com>
2022-06-23 07:53:43 +0530

    GetBit.java Added

---
+++ Java/Bit_Manipulation/GetBit.java
@@ -0,0 +1,9 @@
+// 03. Find i th bit in a binary representation of any decimal number.
+public class GetBit {
+    public static void main(String[] args) {
+        int num = 8, pos = 2;
+        int mask = 1 << pos;
+        int result = num & mask;
+        System.out.println("Bit at position " + pos + " in " + num + " is " + result);
+    }
+}
