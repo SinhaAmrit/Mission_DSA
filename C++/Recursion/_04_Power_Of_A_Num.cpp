86cd8e6d6b9eb580f23785f533124879aeeafbc7
Amrit Sinha <purpleplots@gmail.com>
2022-07-07 16:42:40 +0530

    _04_Power_Of_A_Num.cpp Added

---
+++ C++/Recursion/_04_Power_Of_A_Num.cpp
@@ -0,0 +1,45 @@
+/*
+Problem Statement:
+Ninja is sitting in an examination hall. He is encountered with a problem statement,
+"Find ‘Num’ to the power ‘N’ (i.e. ‘Num’ ^ ‘N’). Where ‘Num’ and ‘N’ are two integers."
+Ninja was not prepared for this question at all, as this question was unexpected in the exam.
+He is asking for your help to solve this problem. Help Ninja to find the answer to the problem.
+Note: For this question, you can assume that 0 raised to the power of 0 is 1.
+
+Input:
+2
+5 2
+9 3
+
+Output:
+1
+25
+729
+
+Constraints:
+0 <= X <= 10
+0 <= num <= 10
+*/
+#include <iostream>
+using namespace std;
+
+long long Pow(int num, int N)
+{
+    if (N == 0)
+        return 1;
+    // long long partialAns = Pow(num, N-1);
+    // return num * partialAns;
+    long long temp = Pow(num, N / 2);
+    if ((N & 1) != 0)
+        return temp * temp * num;
+    return temp * temp;
+}
+
+int main()
+{
+    int num, N;
+    cout << "Enter number and power (seperated by spaces): ";
+    cin >> num >> N;
+    cout << Pow(num, N) << endl;
+    return 0;
+}
