e72dc823fcf826a24b1be7008cf0ff19f8d06ad2
Amrit Sinha <purpleplots@gmail.com>
2022-07-20 14:29:21 +0530

    _13_Ckeck_Palindrome.cpp Modified

--- C++/Recursion/_13_Ckeck_Palindrome.cpp
+++ C++/Recursion/_13_Ckeck_Palindrome.cpp
@@ -5,9 +5,9 @@ Todo: Check whether the given string is palindrome or not (using recursion).
 using namespace std;
 
 bool checkPalindrome(string str, int start , int end){
-if(start > end) return true;
-if(str[start] != str[end]) return false;
-else return checkPalindrome(str, start + 1, end - 1);
+   if(start > end) return true;
+   if(str[start] != str[end]) return false;
+   else return checkPalindrome(str, start + 1, end - 1);
 }
 
 int main()
