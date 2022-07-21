c465fdc7b6fa0ee3a9de01f8b826e56531e15fe1
Amrit Sinha <purpleplots@gmail.com>
2022-07-21 15:49:46 +0530

    _15_Bubble_Sort.cpp Updated

--- C++/Recursion/_15_Bubble_Sort.cpp
+++ C++/Recursion/_15_Bubble_Sort.cpp
@@ -4,18 +4,18 @@ Todo: Implement "Bubble sort" using recursion.
 #include <bits/stdc++.h>
 using namespace std;
 
-void binary_search(int *arr, int size){
+void bubble_search(int *arr, int size){
     if(size==0 || size==1) return;
     for(int i=0; i<size - 1; i++)
         if(arr[i] > arr[i+1])
             swap(arr[i], arr[i+1]);
-    binary_search(arr, size-1);
+    bubble_search(arr, size-1);
 }
 
 int main()
 {
     int arr[5] = {8, 9, 7, 6, 2};
-    binary_search(arr, 5);
+    bubble_search(arr, 5);
     for (int i=0; i<5; i++)
         cout<< arr[i]<<" ";
     cout<< endl;
